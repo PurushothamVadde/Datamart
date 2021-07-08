b38c313921b8cbfd0fa8589c90e589f28ae44616
PurushothamVadde <purushotham.vadde93@gmail.com>
2021-07-08 15:10:40 -0500

    sdf

--- com/pg/source_data_loading.py
+++ com/pg/source_data_loading.py
@@ -1,83 +1,125 @@
-#Read data from mysql- transactionsync and create a dataframe
-#add a column 'ins_dt' Insertion data
-#Write the dataframe in s3 partitined by ind_dt
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import yaml
-import os
+import os.path
 from com.pg.utils import aws_utils as ut
 
 if __name__ == '__main__':
 
+    # create the spark object
     spark = SparkSession\
         .builder\
-        .appName("ingesting data from other sources")\
+        .appName("Data Ingestion from Project Sources")\
         .getOrCreate()
+
+    # to log only the error logs in the console
     spark.sparkContext.setLogLevel('ERROR')
 
+    # Reading the Configuration files
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
-    app_secret_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
-    conf = open(app_secret_path)
-    secret_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
 
+    # Reading Data from S3
     hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
-    hadoop_conf.set("fs.s3a.access.key", secret_conf["s3_conf"]["access_key"])
-    hadoop_conf.set("fs.s3a.secret.key", secret_conf["s3_conf"]["secret_access_key"])
+    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+
 
     src_list = app_conf["source_list"]
 
+
     for src in src_list:
-        src_conf = app_conf[src]
+
         if src == 'SB':
-            txn_df = ut.read_from_sql(spark, src_conf, secret_conf)\
-                .withColumn("ins_dt", current_date())
+            jdbc_params = {"url": ut.get_mysql_jdbc_url(app_secret),
+                           "lowerBound": "1",
+                           "upperBound": "100",
+                           "dbtable": app_conf["mysql_conf"]["dbtable"],
+                           "numPartitions": "2",
+                           "partitionColumn": app_conf["mysql_conf"]["partition_column"],
+                           "user": app_secret["mysql_conf"]["username"],
+                           "password": app_secret["mysql_conf"]["password"]
+                           }
 
-            txn_df.show(5, False)
+            # use the ** operator/un-packer to treat a python dictionary as **kwargs
+            # Read Transactions Data from Mysql
+            tnxDF = spark \
+                .read.format("jdbc") \
+                .option("driver", "com.mysql.cj.jdbc.Driver") \
+                .options(**jdbc_params) \
+                .load()
 
-            txn_df.write\
-                .mode("append") \
-                .partitionBy("ins_dt")\
-                .parquet("s3a://"+app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] +"/" + src)
+            tnxDF = tnxDF.withColumn("ins_dt", current_date())
+            tnxDF.show(5, False)
 
+            # write data to S3 in parquet format
+            tnxDF\
+                .write \
+                .mode("overwrite") \
+                .partitionBy("ins_dt") \
+                .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/" + src)
         elif src == 'OL':
-            ol_txn_df = ut.read_from_sftp(spark, src_conf, secret_conf,
-                                           os.path.abspath(current_dir + "/../../" + secret_conf["sftp_conf"]["pem"]))\
-                .withColumn("ins_dt", current_date())
+            # Reading Data from SFTP server
+            ol_txn_df = spark.read\
+                .format("com.springml.spark.sftp")\
+                .option("host", app_secret["sftp_conf"]["hostname"])\
+                .option("port", app_secret["sftp_conf"]["port"])\
+                .option("username", app_secret["sftp_conf"]["username"])\
+                .option("pem", os.path.abspath(current_dir + "/../../" + app_secret["sftp_conf"]["pem"]))\
+                .option("fileType", "csv")\
+                .option("delimiter", "|")\
+                .load(app_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv")
 
+            ol_txn_df = ol_txn_df.withColumn("ins_dt", current_date())
             ol_txn_df.show(5, False)
+            # write data to S3 in parquet format
+            ol_txn_df\
+                .write \
+                .mode("overwrite") \
+                .partitionBy("ins_dt") \
+                .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/" + src)
 
-            ol_txn_df.write \
-                .mode("append") \
-                .partitionBy("ins_dt")\
-                .parquet("s3a://"+app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] +"/" + src)
 
         elif src == 'CP':
-            cp_df = ut.read_from_s3(spark, src_conf)\
-                .withColumn("ins_dt", current_date())
+            cust_df = spark.read \
+                .option("header", "false") \
+                .option("delimiter", ",") \
+                .option("inferSchema", "true") \
+                .csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/data/KC_Extract_1_20171009.csv") \
 
-            cp_df.show(5, False)
-
-            cp_df.write \
-                .mode("append") \
-                .partitionBy("ins_dt")\
-                .parquet("s3a://"+app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] +"/" + src)
+            cust_df = cust_df.withColumn("ins_dt", current_date())
+            cust_df.show(5)
+            # write data to S3
+            cust_df \
+                .write \
+                .mode("overwrite") \
+                .partitionBy("ins_dt") \
+                .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/" + src)
 
         elif src == 'ADDR':
-            cust_addr_df = ut.read_from_mongoDB(spark, src_conf)\
-                .withColumn("ins_dt", current_date())
+            # Reading from mongodb
+            cust_addr_df = spark\
+                .read\
+                .format("com.mongodb.spark.sql.DefaultSource")\
+                .option("database", app_conf["mongodb_config"]["database"])\
+                .option("collection", app_conf["mongodb_config"]["collection"])\
+                .load()
 
-            cust_addr_df.show(5, False)
+            cust_addr_df = cust_df.withColumn("ins_dt", current_date())
+            cust_addr_df.show(5)
+            # write data to S3
+            cust_addr_df \
+                .write \
+                .mode("overwrite") \
+                .partitionBy("ins_dt") \
+                .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] + "/" + src)
 
-            cust_addr_df.write \
-                .mode("append") \
-                .partitionBy("ins_dt")\
-                .parquet("s3a://"+app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["s3_conf"]["staging_dir"] +"/" + src)
 
 
-
-#spark-submit --packages "mysql:mysql-connector-java:8.0.15, com.springml:spark-sftp_2.11:1.1.1, org.mongodb.spark:mongo-spark-connector_2.11:2.4.1, org.apache.hadoop:hadoop-aws:2.7.4" com/pg/source_data_loading.py
+# spark-submit --packages "mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.apache.hadoop:hadoop-aws:2.7.4,org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" com/pg/source_data_loading.py
