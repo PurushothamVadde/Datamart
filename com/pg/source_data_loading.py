474dd73bd49d54fd6b90072b906cf83d1d9a581d
PurushothamVadde <purushotham.vadde93@gmail.com>
2021-07-24 11:55:58 -0500

    update

--- com/pg/source_data_loading.py
+++ com/pg/source_data_loading.py
@@ -19,7 +19,8 @@ if __name__ == '__main__':
     spark = SparkSession\
         .builder\
         .appName("Data Ingestion from Project Sources")\
-        .config("spark.mongodb.input.uri", app_secret["mongodb_conf"]["uri"])\
+        .config("spark.mongodb.input.uri", app_secret["mongodb_conf"]["uri"]) \
+        .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.11:2.4.1')\
         .getOrCreate()
 
     # to log only the error logs in the console
@@ -98,6 +99,6 @@ if __name__ == '__main__':
 
 
 
-# spark-submit --packages "mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.apache.hadoop:hadoop-aws:2.7.4,org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" com/pg/source_data_loading.py
+# spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1,mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.apache.hadoop:hadoop-aws:2.7.4" com/pg/source_data_loading.py
 
-#spark-submit --packages "mysql:mysql-connector-java:8.0.15," com/pg/source_data_loading.py
+# spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1,org.apache.hadoop:hadoop-aws:2.7.4" com/pg/source_data_loading.py
