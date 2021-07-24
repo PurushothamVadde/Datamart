f0548ffc520edd28586a93a940faf444aac222f8
PurushothamVadde <purushotham.vadde93@gmail.com>
2021-07-24 15:20:55 -0500

    update

--- com/pg/source_data_loading.py
+++ com/pg/source_data_loading.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
 
 # spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1,mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.apache.hadoop:hadoop-aws:2.7.4" com/pg/source_data_loading.py
 
-# spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1,org.apache.hadoop:hadoop-aws:2.7.4" com/pg/source_data_loading.py
+# spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.3.0,org.apache.hadoop:hadoop-aws:2.7.4" com/pg/source_data_loading.py
 
 # .config("spark.mongodb.input.uri", app_secret["mongodb_conf"]["uri"]) \
 # .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.11:2.3.0')
