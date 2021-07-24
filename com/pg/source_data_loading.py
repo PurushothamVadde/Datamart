1b2fda889b5a82bb6133f5bc75bd4c9f193a2434
PurushothamVadde <purushotham.vadde93@gmail.com>
2021-07-24 11:07:50 -0500

    update

--- com/pg/source_data_loading.py
+++ com/pg/source_data_loading.py
@@ -55,8 +55,7 @@ if __name__ == '__main__':
             txn_df = ut.read_from_sftp(spark,
                                        app_secret,
                                        os.path.abspath(current_dir + "/../../" + app_secret["sftp_conf"]["pem"]),
-                                       app_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv")
-
+                                       src_conf["sftp_conf"]["directory"] + "/receipts_delta_GBR_14_10_2017.csv")
 
             txn_df = txn_df.withColumn("ins_dt", current_date())
             txn_df.show(5, False)
