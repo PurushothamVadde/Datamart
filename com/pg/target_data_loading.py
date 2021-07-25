39e013628826d1ef726c01740f901f13b62641e5
PurushothamVadde <purushotham.vadde93@gmail.com>
2021-07-25 10:22:02 -0500

    update

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .appName("Read ingestion enterprise applications") \
-        .master('local[*]') \
         .getOrCreate()
     spark.sparkContext.setLogLevel('ERROR')
 
@@ -37,24 +36,27 @@ if __name__ == '__main__':
     txn_df = spark.read \
         .option("header", "true") \
         .option("delimiter", "|") \
-        .csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/txn_fct.csv")
+        .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"]["staging_dir"]+ "/SB")
 
     txn_df.show(5, False)
 
-    print("Writing txn_fact dataframe to AWS Redshift Table   >>>>>>>")
 
-    jdbc_url = ut.get_redshift_jdbc_url(app_secret)
-    print(jdbc_url)
 
-    txn_df.coalesce(1).write \
-        .format("io.github.spark_redshift_community.spark.redshift") \
-        .option("url", jdbc_url) \
-        .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp") \
-        .option("forward_spark_s3_credentials", "true") \
-        .option("dbtable", "PUBLIC.TXN_FCT") \
-        .mode("overwrite") \
-        .save()
-
-    print("Completed   <<<<<<<<<")
+    #
+    # print("Writing txn_fact dataframe to AWS Redshift Table   >>>>>>>")
+    #
+    # jdbc_url = ut.get_redshift_jdbc_url(app_secret)
+    # print(jdbc_url)
+    #
+    # txn_df.coalesce(1).write \
+    #     .format("io.github.spark_redshift_community.spark.redshift") \
+    #     .option("url", jdbc_url) \
+    #     .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp") \
+    #     .option("forward_spark_s3_credentials", "true") \
+    #     .option("dbtable", "PUBLIC.TXN_FCT") \
+    #     .mode("overwrite") \
+    #     .save()
+    #
+    # print("Completed   <<<<<<<<<")
 
 # spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "org.apache.spark:spark-avro_2.11:2.4.2,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.hadoop:hadoop-aws:2.7.4" dataframe/provision/df_redshift.py
